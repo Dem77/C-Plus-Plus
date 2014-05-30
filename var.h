08fe794b025d41a5b8d45071783081b72049dfab
Dem77 <dimitris.tsioumas89@gmail.com>
2014-05-30 18:52:56 +0300

    Update var.h

--- var.h
+++ var.h
@@ -1,19 +1,46 @@
 class var {
-	void *value;
+	union {
+		long x;
+		double d;
+		char *value;
+	} u;
 	char type;
-	var(int);
+public:
+	var(long);
+	var(double);
 	var(const char *);
 	~var();
+	void display();
 };
-
-var::var(int x)
-{
-	value = new int;
-	*(int *) value=x;
+var::var(long x)
+{   type='L';
+	u.x = x;
+}
+var::var(double x)
+{   type='D';
+	u.d = x;
 }
 
 var::var(const char *s)
-{
-	value= new char[strlen(s)+1];
-	strcpy((char *)value,s);
+{   type='S';
+	u.value= new char[strlen(s)+1];
+	strcpy(u.value,s);
 }
+var::~var()
+{    if (type=='S')
+		delete u.value;
+}
+void var::display()
+{
+	switch(type)
+	{ case 'L':
+		cout << u.x;
+		break;
+	  case 'D':
+		cout << u.d;
+		break;
+	  case 'S':
+		cout << u.value;
+	}
+}
+
